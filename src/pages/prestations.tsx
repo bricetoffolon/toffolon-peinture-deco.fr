32b1fb713526237b4066c5432e85e5fba09c31e5
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-06-20 19:25:42 +0200

    feat: add prestations page

--- src/pages/prestations.tsx
+++ src/pages/prestations.tsx
@@ -1,10 +1,15 @@
-import NavBar from "@/components/layout/NavBar";
+import React from "react";
 
-export default function Prestations(): any
-{
+import NavBar from "@/components/layout/NavBar";
+import Prestations from "@/components/prestations";
+import Footer from "@/components/layout/Footer";
+
+export default function PrestationsPage(): React.JSX.Element {
     return (
         <>
             <NavBar />
+            <Prestations />
+            <Footer />
         </>
     );
 }
