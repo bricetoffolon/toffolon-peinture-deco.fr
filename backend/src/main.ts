67fd78e23f60165c46b29da83ba594ce74d3ed11
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-06-28 19:17:42 +0200

    feat: add prisma integration to project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+import {PrismaService} from "./prisma/prisma.service";
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
+  const primaService = app.get(PrismaService);
+  await primaService.enableShutdownHooks(app);
 }
 bootstrap();
