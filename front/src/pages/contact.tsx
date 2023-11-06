0b77e223821b9e6de38fb22c31ddf01833245062
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-06 22:48:11 +0100

    feat: add new branch feat/page/contact

--- front/src/pages/contact.tsx
+++ front/src/pages/contact.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import NavBar from "@/components/layout/NavBar";
 import Footer from "@/components/layout/Footer";
+import Contact from "@/components/contact";
 
 export default function contactPage(): React.JSX.Element {
     return (
         <>
             <NavBar/>
+            <Contact/>
             <Footer/>
         </>
     );
