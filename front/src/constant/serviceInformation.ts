ed6f5c4215ec09bd88ec692d58a36ffe33e46164
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 17:45:30 +0100

    fix: replace json with constant file

---
+++ front/src/constant/serviceInformation.ts
@@ -0,0 +1,76 @@
+import React from "react";
+
+import { FaCalendarCheck, FaCogs, FaFileContract, FaTruck } from 'react-icons/fa';
+
+interface step {
+    title: string,
+    label: string,
+    Icon: React.ElementType
+}
+
+interface content {
+    title: string,
+    label: string,
+    steps: step[]
+}
+
+interface service {
+    name: string,
+    imageUrl: string,
+    content?: content
+}
+
+export const serviceInformation: service[] =
+    [
+        {
+            name: 'Ravalement',
+            imageUrl:
+                'https://toffolon-website.s3.eu-west-3.amazonaws.com/services/DALL%C2%B7E+2023-11-18+13.28.55+-+A+16_9+format+full-frame+image+featuring+a+flat+vector+design+of+a+building+renovation%2C+centered+on+a+white+background.+The+design+is+simple+and+effic.png',
+        },
+        {
+            name: 'Peinture',
+            imageUrl:
+                'https://toffolon-website.s3.eu-west-3.amazonaws.com/services/DALL%C2%B7E+2023-11-18+13.32.09+-+A+16_9+format+full-frame+image+with+a+white+background%2C+featuring+a+simple+and+efficient+flat+vector+design+of+painting+work+on+a+building%2C+centered+i.png',
+        },
+        {
+            name: 'Rénovation',
+            imageUrl:
+                'https://toffolon-website.s3.eu-west-3.amazonaws.com/services/DALL%C2%B7E+2023-11-18+13.34.51+-+A+16_9+format+full-frame+image+with+a+white+background%2C+featuring+a+flat+vector+design+of+renovation+work+inside+a+building%2C+including+painting%2C+cente.png',
+        },
+        {
+            name: 'Isolation thermique',
+            imageUrl:
+                'https://toffolon-website.s3.eu-west-3.amazonaws.com/services/DALL%C2%B7E+2023-11-18+13.30.02+-+A+16_9+format+full-frame+image+with+a+white+background%2C+featuring+a+flat+vector+design+centered+on+building+thermal+insulation.+The+design+is+simple+a.png',
+            content: {
+                title: 'Isolation Thermique',
+                label: 'Notre entreprise est certifiée QUALIBAT RGE depuis 2015 pour la réalisation des travaux d’isolation thermique exté- rieure sur les pavillons, im- meubles, magasins...',
+                steps: [
+                    {
+                        title: 'Analyse',
+                        label: 'Etude de votre projet & Conseil Techniques',
+                        Icon: FaCogs,
+                    },
+                    {
+                        title: 'Planification',
+                        label: 'Remise des Document légaux & Planning prévisionnel',
+                        Icon: FaCalendarCheck,
+                    },
+                    {
+                        title: 'Lancement',
+                        label: 'Commande des matériaux & Début du chantier',
+                        Icon: FaTruck,
+                    },
+                    {
+                        title: 'Finalisation',
+                        label: 'Signature du PV de réception & Remise des garanti si nécessaires',
+                        Icon: FaFileContract,
+                    },
+                ],
+            },
+        },
+        {
+            name: 'Revêtements de sols',
+            imageUrl:
+                'https://toffolon-website.s3.eu-west-3.amazonaws.com/services/DALL%C2%B7E+2023-11-18+13.31.07+-+A+16_9+format+full-frame+image+with+a+white+background%2C+featuring+a+centered+flat+vector+design+of+a+building+floor+renovation.+The+design+is+simple+a.png',
+        },
+    ];
