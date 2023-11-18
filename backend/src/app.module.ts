09ab042ec1de021e1338fef3b84d72ed79f60f62
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 01:54:10 +0100

    feat: add backend rate limit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,22 @@ import { JwtTokenModule } from './JwtToken/jwtToken.module';
 import { PostModule } from './post/post.module';
 import { ContactModule } from './contact/contact.module';
 import { StartModule } from "./start/start.module";
+import {ThrottlerModule} from "@nestjs/throttler";
 
 @Module({
-  imports: [AuthModule, MailModule, UsersModule, PrismaModule, JwtTokenModule, PostModule, ContactModule, StartModule],
+  imports: [
+    AuthModule,
+    MailModule,
+    UsersModule,
+    PrismaModule,
+    JwtTokenModule,
+    PostModule,
+    ContactModule,
+    StartModule,
+    ThrottlerModule.forRoot([{
+      ttl: 6000,
+      limit: 10,
+    }])],
   controllers: [],
 })
 export class AppModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,10 +4,18 @@ import { PrismaModule } from "../prisma/prisma.module";
 import { UsersController } from "./users.controller";
 import { MailModule } from "../mail/mail.module";
 import { JwtTokenModule } from "../JwtToken/jwtToken.module";
+import {ThrottlerGuard} from "@nestjs/throttler";
+import {APP_GUARD} from "@nestjs/core";
 
 @Module({
   imports: [PrismaModule, MailModule, JwtTokenModule],
-  providers: [UsersService],
+  providers: [
+      UsersService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   controllers: [UsersController],
   exports: [UsersService],
 })
