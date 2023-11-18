b7e16842d5200491e9e284bf2e3bdf68024510ec
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 01:12:16 +0100

    feat: add output from backend and delete useless element

--- backend/src/contact/create-contact.dto.ts
+++ backend/src/contact/create-contact.dto.ts
@@ -16,15 +16,5 @@ export class CreateContactDto {
 
     @IsString()
     @IsNotEmpty()
-    phone_number: string
-
-    @IsString()
-    which_benefit: string
-
-    @IsString()
-    how_you_heard_about_us: string
-
-    @IsString()
-    @IsNotEmpty()
     message: string
 }
