2ffc5815a0e6395137f3361724d0dc4b91d13670
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 17:46:53 +0100

    feat: add new service card landing

---
+++ front/src/components/services/serviceCardLanding.tsx
@@ -0,0 +1,52 @@
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Flex,
+    Heading,
+    Image,
+    Button,
+} from '@chakra-ui/react';
+import React from 'react';
+
+export default function ServiceCardLanding({ service, isOpen, setIsOpen }) {
+    return (
+        <Card
+            style={{
+                scrollSnapAlign: 'start',
+            }}
+            borderColor="whiteAlpha.500"
+            variant={{
+                base: 'elevated',
+                xl: 'unstyled',
+            }}
+            h="100vh"
+        >
+            <CardHeader>
+                <Flex alignItems="center" direction="column" gap={6} m="1%">
+                    <Heading color="brand.500" size="xl">
+                        {service.name}
+                    </Heading>
+                    <Button onClick={() => setIsOpen(!isOpen)}>En savoirs plus</Button>
+                </Flex>
+            </CardHeader>
+            <CardBody>
+                <Image
+                    src={service.imageUrl}
+                    alt={service.name}
+                    borderRadius="lg"
+                    w={{
+                        base: '100vw',
+                        xl: '70vw',
+                    }}
+                    h={{
+                        base: '30vh',
+                        xl: '70vh',
+                    }}
+                />
+            </CardBody>
+            <CardFooter />
+        </Card>
+    );
+}
