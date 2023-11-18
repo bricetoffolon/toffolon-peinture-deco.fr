31fb272ec6cf2996fea8b52d898c98882569252d
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 14:53:22 +0100

    feat: fix eslint in project

--- front/src/constant/informationData.tsx
+++ front/src/constant/informationData.tsx
@@ -1,22 +1,18 @@
-import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa';
 
 export default function informationData() {
-    const informationData = [
+    return [
         {
-            'Icon': FaPhoneAlt,
-            'text': 'Téléphone',
-            'content': '+33 1 46 71 84 19',
-            'catchPhrase': 'Une question ? Appelez-nous !'
+            Icon: FaPhoneAlt,
+            text: 'Téléphone',
+            content: '+33 1 46 71 84 19',
+            catchPhrase: 'Une question ? Appelez-nous !',
         },
         {
-            'Icon': FaMapMarkerAlt,
-            'text': 'Addresse',
-            'content': '25 allée gabriel, 93250, Les pavillons-sous-bois',
-            'catchPhrase': 'Où nous sommes ?'
-        }
-    ]
-
-    return (
-        informationData
-    );
+            Icon: FaMapMarkerAlt,
+            text: 'Addresse',
+            content: '25 allée gabriel, 93250, Les pavillons-sous-bois',
+            catchPhrase: 'Où nous sommes ?',
+        },
+    ];
 }
