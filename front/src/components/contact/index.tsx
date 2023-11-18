48821046a59b267f628aeae99407ea3ecc22d17e
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2023-11-18 01:16:13 +0100

    feat: add updated index for contact

--- front/src/components/contact/index.tsx
+++ front/src/components/contact/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container, Flex, Heading, Text} from "@chakra-ui/react";
+import {Container, Flex, Heading, Text} from "@chakra-ui/react";
 import ContactForm from "@/components/contact/ContactForm";
 import ContactInformation from "@/components/contact/ContactInformation";
 
+import { motion } from "framer-motion";
+
 export default function Contact(): React.JSX.Element {
     return (
-        <Container maxW={"full"} overflow={"hidden"} h={'100vh'} padding={"3%"}>
+        <Container
+            maxW={"full"}
+            overflow={{
+                base: undefined,
+                xl: "hidden",
+            }}
+            h={{
+                base: undefined,
+                xl: "100vh"
+            }}
+            padding={"3%"}
+        >
             <Flex
                 direction={"column"}
                 gap={"10%"}
@@ -15,15 +28,41 @@ export default function Contact(): React.JSX.Element {
                    alignSelf={"center"}
                    alignItems={"center"}
                >
-                   <Heading
-                       size={"4xl"}
+                   <motion.div
+                       whileHover={{ scale: 1.2 }}
+                       whileTap={{ scale: 1.1 }}
+                       initial={{ y: -150, filter: "blur(2px)" }}
+                       animate={{ y: 0, filter: "blur(0px)" }}
+                       transition={{ duration: 0.8, ease: "easeOut" }}
                    >
-                       Contactez-nous dès <Heading size={"4xl"} as={"span"} color={"brand.500"}>maintenant !</Heading>
-                   </Heading>
-                   <Text mt="2%" as="b" fontSize="4xl" color={"gray.400"}>Pour un devis gratuit et personnalisé, <br/>n'hésitez pas à prendre contact avec nous</Text>
+                       <Heading
+                           size={{
+                               base: "xl",
+                               xl: "4xl"
+                           }}
+                       >
+                           Contactez-nous dès <Heading size={{base: "xl", xl: "4xl"}} as={"span"} color={"brand.500"}>maintenant !</Heading>
+                       </Heading>
+                   </motion.div>
+                   <Flex
+                       mt="2%" mb="2%"
+                   >
+                       <motion.div
+                           whileHover={{ scale: 1.1 }}
+                           whileTap={{ scale: 0.9 }}
+                           initial={{ x: -50, filter: "blur(2px)" }}
+                           animate={{ x: 0, filter: "blur(0px)" }}
+                           transition={{ duration: 0.8, ease: "easeOut" }}
+                       >
+                           <Text as="b" fontSize={{base: "xl", xl: "4xl"}} color={"brand.400"}>Pour un devis gratuit et personnalisé, <br/>n'hésitez pas à prendre contact avec nous</Text>
+                       </motion.div>
+                    </Flex>
                </Flex>
                 <Flex
-                    gap={"justify-space"}
+                    direction={{
+                        base: "column",
+                        xl: "row"
+                    }}
                 >
                     <ContactForm />
                     <ContactInformation/>
