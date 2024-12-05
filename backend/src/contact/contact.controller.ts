f3d1440829ad947dc9bee03c6cb9fd835e3b0565
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2024-12-05 17:06:13 +0100

    refactor: contact method to align with refactored front

--- backend/src/contact/contact.controller.ts
+++ backend/src/contact/contact.controller.ts
@@ -1,40 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as ejs from "ejs";
 import MailService from "../mail/mail.service";
 import { CreateContactDto } from "./create-contact.dto";
-import { ConfigService } from "@nestjs/config";
-import * as ejs from 'ejs';
 
-@Controller('contact')
+@Controller("contact")
 export class ContactController {
-    private readonly serverEmail;
+  private readonly serverEmail;
 
-    constructor(
-        private mailService: MailService,
-        private configService: ConfigService
-    ) {
-        this.serverEmail = configService.get<string>('EMAIL');
-    }
+  constructor(
+    private mailService: MailService,
+    private configService: ConfigService,
+  ) {
+    this.serverEmail = configService.get<string>("EMAIL");
+  }
 
-    @Post('/')
-    async putInContact( @Body() createContactDto: CreateContactDto): Promise<{ message: string }> {
-        const { last_name, first_name, email, message } = createContactDto;
+  @Post("/")
+  async putInContact(
+    @Body() createContactDto: CreateContactDto,
+  ): Promise<{ message: string }> {
+    const { username, email, message } = createContactDto;
 
-        const username = `${first_name} ${last_name}`
+    const informationHtml = await ejs.renderFile(
+      "src/contact/views/contact-informations-template-email.ejs",
+      { username, message, email },
+    );
 
-        const informationHtml = await ejs.renderFile(
-            "src/contact/views/contact-informations-template-email.ejs",
-            {"username": username, "message": message, "email": email }
-        )
+    await this.mailService.sendEmail(
+      this.serverEmail,
+      "Toffolon - Nouveau contact !",
+      informationHtml,
+    );
 
-        await this.mailService.sendEmail(this.serverEmail, "Toffolon - Nouveau contact !", informationHtml)
+    const confirmationHtml = await ejs.renderFile(
+      "./src/contact/views/contact-template-email.ejs",
+      { username },
+    );
 
-        const confirmationHtml = await ejs.renderFile(
-            "./src/contact/views/contact-template-email.ejs",
-            {"username": username},
-        );
+    await this.mailService.sendEmail(
+      email,
+      `Toffolon - Prise de contact`,
+      confirmationHtml,
+    );
 
-        await this.mailService.sendEmail(email, `Toffolon - Prise de contact`, confirmationHtml);
-
-        return {"message": "Votre message a été envoyé"}
-    }
+    return { message: "Votre message a été envoyé" };
+  }
 }
