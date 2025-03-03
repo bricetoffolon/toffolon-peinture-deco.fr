477a26450994f42574259fb8dc8249feb83a0b9a
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2025-03-03 14:24:54 +0000

    feat(front): add protected routes

--- front/src/pages/redondo/index.tsx
+++ front/src/pages/redondo/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBarDashboard from '@/components/layout/NavBar/NavBarDashboard';
 import Dashboard from '@/components/api/dashboard/Dashboard';
+import ProtectedRoute from "@/components/api/user/protectedRoute";
 
 export default function DashboardPage(): React.JSX.Element {
     return (
         <>
             <NavBarDashboard />
-            <Dashboard />
+            <ProtectedRoute>
+                <Dashboard />
+            </ProtectedRoute>
         </>
     );
 }
--- front/src/pages/redondo/service/posts.tsx
+++ front/src/pages/redondo/service/posts.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Posts from '@/components/api/service/posts';
 import NavBarDashboard from '@/components/layout/NavBar/NavBarDashboard';
+import ProtectedRoute from "@/components/api/user/protectedRoute";
 
 export default function PostsPage(): React.JSX.Element {
     return (
         <>
             <NavBarDashboard />
-            <Posts />
+            <ProtectedRoute>
+                <Posts />
+            </ProtectedRoute>
         </>
     );
 }
