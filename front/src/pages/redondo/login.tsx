9e6b8935b3adb98130d39ca42c0d6f523a3158c9
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2025-03-03 14:27:32 +0000

    fix(front): import in uppercase instead of lowercase

--- front/src/pages/redondo/login.tsx
+++ front/src/pages/redondo/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBarDashboard from '@/components/layout/NavBar/NavBarDashboard';
-import Login from '@/components/api/user/Login';
+import Login from '@/components/api/user/login';
 
 export default function LoginPage(): React.JSX.Element {
     return (
