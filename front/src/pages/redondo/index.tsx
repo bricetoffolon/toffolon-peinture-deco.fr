98333f5fbc9588601d04cad44cc37e2513e0ed44
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2025-03-04 11:06:47 +0000

    feat(front): add footer to api landing page

--- front/src/pages/redondo/index.tsx
+++ front/src/pages/redondo/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import NavBarApi from '@/components/layout/NavBar/navBarApi';
 import Dashboard from '@/components/api/dashboard/Dashboard';
 import ProtectedRoute from "@/components/api/user/protectedRoute";
+import {Flex} from "@chakra-ui/react";
+import Footer from "@/components/layout/Footer";
 
 export default function DashboardPage(): React.JSX.Element {
     return (
         <>
-            <NavBarApi />
-            <ProtectedRoute>
-                <Dashboard />
-            </ProtectedRoute>
+            <Flex direction={"column"} h={"100vh"}>
+                <NavBarApi />
+                <Flex direction={"column"} flex={"1"}>
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                </Flex>
+            </Flex>
+            <Footer />
         </>
     );
 }
