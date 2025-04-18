26a71bbbc72d8075f7accf4a276c1beea7f10c25
Brice Toffolon <57350282+bricetoffolon@users.noreply.github.com>
2025-04-18 17:46:28 +0200

    chore: add redis to store cookie session

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -10,11 +10,16 @@ import { UserSerializer } from "./user.serializer";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants} from "./constants";
 import { tokenGuard } from "../JwtToken/token-auth/token.guard";
+import { createClient } from 'redis';
+import {RedisStore} from "connect-redis"
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
   imports: [UsersModule,
       PassportModule.register({
           session: true
+      }),
+      ConfigModule.forRoot({
       }),
       JwtModule.register({
           global: true,
@@ -26,15 +31,46 @@ import { tokenGuard } from "../JwtToken/token-auth/token.guard";
   providers: [AuthService, LocalStrategy, UserSerializer, tokenGuard],
   exports: [tokenGuard]
 })
+
 export class AuthModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
+    private readonly sessionSecret;
+    private readonly redisUrl;
+    private readonly sessionMaxAge;
+    private readonly isProduction;
+
+    constructor(private configService: ConfigService) {
+        this.redisUrl = this.configService.get<string>('REDIS_URL');
+        this.sessionSecret = this.configService.get<string>('SESSION_SECRET');
+        this.sessionMaxAge = this.configService.get<number>('SESSION_MAX_AGE', 30 * 60 * 1000);
+        this.isProduction = this.configService.get<string>('NODE_ENV') === 'production';
+    }
+
+    async configure(consumer: MiddlewareConsumer) {
+        const redisClient = createClient({
+            legacyMode: false, // Remove legacyMode for Redis v4+
+            url: this.redisUrl,
+        });
+
+        await redisClient.connect().catch(console.error);
+
+        const redisStore = new RedisStore({
+            client: redisClient,
+            prefix: "session-store:",
+        });
+
         consumer
             .apply(
                 expressSession({
-                    secret: 'SOME SESSION SECRET',
+                    store: redisStore,
+                    secret: this.sessionSecret,
                     resave: false,
                     saveUninitialized: false,
+                    cookie: {
+                        secure: this.isProduction,
+                        maxAge: this.sessionMaxAge, // 30 minutes
+                    }
                 }),
+                passport.initialize(),
                 passport.session(),
             )
             .forRoutes('*')
